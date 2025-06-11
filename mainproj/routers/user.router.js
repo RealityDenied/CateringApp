06b1a504046690daba326fe376731a5a68d74cc2
Vikash Kumar <vikashkumar3791kzq@gmail.com>
2025-06-11 15:47:22 +0530

    Comments added for explanation

--- mainproj/routers/user.router.js
+++ mainproj/routers/user.router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const UserController = require("../controller/user.controller");
 
 router.post('/registration',UserController.register);
+//when reg path hit by client with a req.body json , the register function in UserController is called
 
 module.exports = router;
