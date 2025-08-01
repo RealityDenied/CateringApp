de600d0369d7fcb8575d44700887a2deb91625b5
Vikash Kumar <vikashkumar3791kzq@gmail.com>
2025-08-01 17:54:10 +0530

    backend link added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [react()],
-    server: {
-      allowedHosts: 'all',
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_URL || 'http://localhost:3000',
-          changeOrigin: true,
-        },
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // allowedHosts: ['.ngrok-free.app'],
+    allowedHosts: 'all',
+
+    proxy: {
+      '/api': {
+        target: 'https://cateringapp-dslb.onrender.com' || 'http://localhost:3000',
+        changeOrigin: true,
+        // secure: false, // only if you run backend under HTTPS locally
       },
     },
-  };
-});
+  },
+})
