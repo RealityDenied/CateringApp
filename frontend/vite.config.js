6a9841b005f79a8dcffc3f75618ed4ad808cddf7
Vikash Kumar <vikashkumar3791kzq@gmail.com>
2025-08-01 17:51:16 +0530

    minor deployment edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // allowedHosts: ['.ngrok-free.app'],
-    allowedHosts: 'all',
-
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',  
-        changeOrigin: true,
-        // secure: false, // only if you run backend under HTTPS locally
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: 'all',
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  };
+});
